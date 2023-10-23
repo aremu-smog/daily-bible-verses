ab99708c9824db2037ee752727f45caa9936aa92
Aremu Smog <aremuoluwagbamila@gmail.com>
2023-10-23 18:14:52 +0100

    feat: render sample bible text

--- apps/mobile-app/App.tsx
+++ apps/mobile-app/App.tsx
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import { StyleSheet, Text, View } from "react-native"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text style={styles.bibleVerse}>
+				This is the day that the Lord has made; let us rejoice and be glad in
+				it.
+			</Text>
+			<StatusBar style='auto' />
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: "#222222",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	bibleVerse: {
+		fontSize: 24,
+		color: "#f8f8f8",
+	},
+})
